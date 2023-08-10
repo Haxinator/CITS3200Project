6d36cd123402367720cf3a7fc7bab77100f56bab
Haxinator <joshuarachpaul@hotmail.com>
2023-08-10 14:30:15 +0800

    Rows added when unit dragged under table. Better unit swapping. Some minor graphic fixes/improvements.

--- app/static/js/planner.js
+++ app/static/js/planner.js
@@ -1,4 +1,6 @@
-makeTable(5);
+var year = 0; //may improve somehow.
+
+makeTable(3);
 
 function addToRoot(element)
 {
@@ -10,6 +12,22 @@ function addCellEvents(item)
 {
     item.addEventListener("dragstart", dragstart);
     item.addEventListener("dragend", dragend);
+}
+
+function makeSensor()
+{
+    sensor = document.createElement("div");
+    text = document.createElement("h2");
+    sensor.setAttribute("class", "sensor");
+
+    sensor.addEventListener("dragover", dragover);
+    sensor.addEventListener("dragenter", dragenter);
+    sensor.addEventListener("dragleave", dragleave);
+    sensor.addEventListener("drop", appendRow);
+
+    text.innerHTML = "drag a unit here to add a row!";
+    sensor.appendChild(text);
+    return sensor;
 }
 
 function makeCell(rowNum, colNum)
@@ -32,9 +50,9 @@ function makeRow(rowNum)
     let container = document.createElement("div");
     let head = document.createElement("th");
     let semesterNum = (rowNum % 3);
-    let yearNum = Math.trunc((rowNum/3) + 1);
+   // let yearNum = Math.trunc((rowNum/3) + 1);
     let semesterID = "S" + semesterNum;
-    let yearID = "Y" + yearNum;
+    let yearID = "Y" + year;
 
 
     head.innerHTML = semesterID;
@@ -55,12 +73,39 @@ function makeRow(rowNum)
     return row;
 }
 
-function makeYearRow(yearNum)
+function makeEmptyRow(rowNum)
+{
+    //instead of passing rowNum, pass number of semesters
+
+    let row = document.createElement("tr");
+    let container = document.createElement("div");
+    let head = document.createElement("th");
+    let semesterNum = (rowNum % 3);
+    //let yearNum = Math.trunc((rowNum/3) + 1);
+    let semesterID = "S" + semesterNum;
+    let yearID = "Y" + year;
+
+
+    head.innerHTML = semesterID;
+    container.setAttribute("id", yearID + semesterID);
+    row.appendChild(head);
+    row.appendChild(container);
+
+    container.addEventListener("dragover", dragover);
+    container.addEventListener("dragenter", dragenter);
+    container.addEventListener("dragleave", dragleave);
+    container.addEventListener("drop", drop);
+
+    return row;
+}
+
+function makeYearRow()
 {
     let row = document.createElement("tr");
     let head = document.createElement("th");
-
-    head.innerHTML = "Y" + (yearNum+1);
+    
+    year++;
+    head.innerHTML = "Y" + year;
     head.setAttribute("colspan", "5");
     row.appendChild(head);
     
@@ -70,44 +115,53 @@ function makeYearRow(yearNum)
 function makeTable(CourseDuration)
 {
     let table = document.createElement("table");
+    table.setAttribute("id", "table");
 
     for(let i = 0; i<CourseDuration * 3; i++)
     {
         if(i % 3 == 0)
         {
-            table.appendChild(makeYearRow(i/3));
+            table.appendChild(makeYearRow());
         } else {
             table.appendChild(makeRow(i));
         }
     }
 
+
+    //make table then sensor underneath
     addToRoot(table);
+    addToRoot(makeSensor());
 }
 
 //--------------------- EVENT LISTENER FUNCTIONS -------------------------//
 
+//if your cursor (whilst dragging unit) is over the row add red lines.
 function dragover(e)
 {
     //prevent default to have drop cursor appear
     e.preventDefault();
-    e.currentTarget.classList.add("dragover");
+    e.target.classList.add("dragover");
 }
 
+//if your cursor (whilst dragging unit) enters the row add red lines.
 function dragenter(e)
 {
     e.preventDefault();
-    e.currentTarget.classList.add("dragover");
+    e.target.classList.add("dragover");
 }
 
+//if your cursor (whilst dragging unit) leaves the row, remove red lines.
 function dragleave(e)
 {
-    e.currentTarget.classList.remove("dragover");
+    e.target.classList.remove("dragover");
 }
 
+//When the item is dropped either place the unit in the row or swap it
+//if the row is full with the unit user is hovering over.
 function drop(e)
 {
     //currentTarget used instead of target to prevent cells being dropped into cells.
-    e.currentTarget.classList.remove("dragover");
+    e.target.classList.remove("dragover");
 
     //element id that was stored in datatransfer when drag started
     let id = e.dataTransfer.getData('text/plain');
@@ -124,13 +178,7 @@ function drop(e)
             e.target.appendChild(item);
         }
     } else {
-        //condition if hovering over cell
-        if(e.currentTarget.childElementCount < 4)
-        {
-            //insert item in the container before current cell user is targeting
-            e.currentTarget.insertBefore(item, e.target);
-        } else {
-            // swap units if container full
+            // swap units
             //create clone elements.
             let targetClone = e.target.cloneNode(true);
             let itemClone = item.cloneNode(true); 
@@ -145,7 +193,6 @@ function drop(e)
             //add the event listeners to swapped units.
             addCellEvents(targetClone);
             addCellEvents(itemClone);
-        }
     }
 
     //show item
@@ -159,8 +206,30 @@ function dragstart(e)
     setTimeout(() => e.target.classList.add("hide"), 0);
 }
 
+//When drag ends, reveal the item.
 function dragend(e)
 {
     //put item back if not drop occured at end of drag.
     e.target.classList.remove("hide");
+}
+
+//adds a new row underneath the last row in the table when item dropped beneath row.
+function appendRow(e)
+{
+    //remove lines
+    e.target.classList.remove("dragover");
+    let table = document.getElementById("table");
+
+    //get currently dragged unit.
+    let id = e.dataTransfer.getData('text/plain');
+    let item = document.getElementById(id);
+
+    //make year and semester 1 and 2 rows.
+    table.appendChild(makeYearRow());
+    table.appendChild(makeEmptyRow(1));
+    table.appendChild(makeEmptyRow(2));
+
+    //add dragged unit in S1 of the new row
+    let semester1 = document.getElementById("Y" + year +"S1");
+    semester1.appendChild(item);
 }
