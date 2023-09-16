5d52a8e5b22b53e74000947010adc18bc0014f5f
gen-ie <caraangenevie@gmail.com>
2023-09-16 13:37:21 +0800

    implementing diff types for diff majors

--- app/routes.py
+++ app/routes.py
@@ -50,32 +50,17 @@ def send_unit_information(major, bridging):
     units = bridging.split(",") 
     unit_conditions = " OR ".join([f"u.unitcode = '{unit}'" for unit in units])
 
-    # if major does not have option units (soft)
-    if(major == "SP-ESOFT"):
-        query=f""" MATCH (u)
-        WHERE u.major CONTAINS $major OR {unit_conditions}
-        OPTIONAL MATCH (u)-[:REQUIRES]->(m)
-        WITH u, COLLECT(m.unitcode) as unit_req
-        RETURN u.unitcode as unitcode, u.unitname as unitname, u.type as type, u.semester as semester, u.major as major, u.level as level, u.credit_points as credit_points, u.points_req as points_req, u.enrolment_req as enrolment_req, unit_req, u.incompatible_units as incompatibilities, u.corequisites as corequisites
-        ORDER BY level
-        """
-        x = {"major":major} 
-        results=session.run(query,x)
-        data=results.data()
-        return(jsonify(data))
-    # if major does have option units (mech)
-    else:
-        query=f""" MATCH (u)
-        WHERE u.major CONTAINS $major AND u.type = "CORE" OR {unit_conditions}
-        OPTIONAL MATCH (u)-[:REQUIRES]->(m)
-        WITH u, COLLECT(m.unitcode) as unit_req
-        RETURN u.unitcode as unitcode, u.unitname as unitname, u.type as type, u.semester as semester, u.major as major, u.level as level, u.credit_points as credit_points, u.points_req as points_req, u.enrolment_req as enrolment_req, unit_req, u.incompatible_units as incompatibilities, u.corequisites as corequisites
-        ORDER BY level
-        """
-        x = {"major":major}
-        results=session.run(query,x)
-        data=results.data()
-        return(jsonify(data))
+    query=f""" MATCH (u)
+    WHERE u.type CONTAINS "CORE_$major" OR {unit_conditions}
+    OPTIONAL MATCH (u)-[:REQUIRES]->(m)
+    WITH u, COLLECT(m.unitcode) as unit_req
+    RETURN u.unitcode as unitcode, u.unitname as unitname, u.type as type, u.semester as semester, u.major as major, u.level as level, u.credit_points as credit_points, u.points_req as points_req, u.enrolment_req as enrolment_req, unit_req, u.incompatible_units as incompatibilities, u.corequisites as corequisites
+    ORDER BY level
+    """
+    x = {"major":major} 
+    results=session.run(query,x)
+    data=results.data()
+    return(jsonify(data))
 
 @app.route("/option_units=<string:major>", methods=["GET"])
 def get_option_units(major):
