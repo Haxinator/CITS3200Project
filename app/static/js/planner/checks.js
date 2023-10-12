47d439e0cd6383e520f13c2be1bd4dd00b896662
Haxinator <joshuarachpaul@hotmail.com>
2023-10-12 19:24:14 +0800

    Portfolio gives error when not in final semester of final year.

--- app/static/js/planner/checks.js
+++ app/static/js/planner/checks.js
@@ -13,9 +13,10 @@
  * This file can be hard to understand. Best to ask Josh.
 */
 
-import { getAllUnitInfo } from "./support.js";
+import { getAllUnitInfo, getById, getByPeriod } from "./support.js";
 import { infoBar } from "./classes.js";
 import { highlightIfUnitHasProblems, isAlpha, inDOM, getLastCharacter, getUnitInformation, isOption, comparePeriods } from "./support.js";
+import { planner } from "./main.js";
 
 // --------------- Prerequisite Met Functions ----------------//
 
@@ -42,6 +43,44 @@ export function unitConditionsMet(unitCode, container)
         // colour magenta if problem found
         highlightIfUnitHasProblems(unit);
     }
+
+    //hardcode check if portfolio in right spot
+    if(unitCode.includes("GENG5010"))
+    {
+        //if in planner and planner in DOM.
+        if(unit.isEnrolled() && planner != undefined)
+        {
+            let lastYear = getById("table").lastElementChild.id[1];
+            let yearCorrect = lastYear == container.id[1];
+            console.log(getByPeriod(lastYear, "NS2").lastElementChild);
+            console.log(getByPeriod(lastYear, "S2").lastElementChild);
+
+            let semesterCorrect = container.id[4] == "2" || (getByPeriod(lastYear, "NS2").lastElementChild == null && getByPeriod(lastYear, "S2").lastElementChild == null);
+
+            if(yearCorrect && semesterCorrect)
+            {
+                return true;
+            } else {
+                unit.problems.push(`You can only enroll into ${unitCode} in your last semester of your last year!`);
+                infoBar.addInfo(`You can only enroll into ${unitCode} in your last semester of your last year!`);
+                highlightIfUnitHasProblems(unit);
+
+                return false;
+            }
+
+
+        } else {
+            for(let thatUnit of planner.unitInformation.values())
+            {
+                if(!thatUnit.isEnrolled() && thatUnit.unitCode != unitCode)
+                {
+                    return false;
+                }
+            }
+        }
+
+    }
+
 
     return correctSemester && correctPrequisites && correctCorequisites && correctPoints;
 }
