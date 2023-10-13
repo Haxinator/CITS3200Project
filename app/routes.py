c4995d7a51737031a0aa146009a89a0c7e334821
gen-ie <caraangenevie@gmail.com>
2023-10-14 02:32:47 +0800

    added option combos for civil + some documentation

--- app/routes.py
+++ app/routes.py
@@ -40,6 +40,9 @@ def preferences():
 
 
 # ------------------------------------- NEO4J QUERIES ------------------------------------------------
+"""
+Creates the driver to connect application to remote database and run queries with it
+"""
 driver = GraphDatabase.driver(
     "bolt://e5218dc4.databases.neo4j.io:7687", 
     auth=("neo4j", "svH9RLz19fQFpDDgjnQCZMO9MF6WEVPRmtpXEaNVQ2o"), 
@@ -47,6 +50,10 @@ driver = GraphDatabase.driver(
     trust="TRUST_ALL_CERTIFICATES"
 )
 
+"""
+Query: Get all majors that offers a specified year
+Return: dataframe containing the major code and its name
+"""
 @app.route("/get_majors=<string:year>", methods=["GET"])
 def get_majors(year):
     with driver.session() as session:
@@ -60,7 +67,10 @@ def get_majors(year):
         data = results.data()
         return jsonify(data)
     
-    
+"""
+Query: Get the maximum broadening points of a specified major
+Return: maximum broadening points property of specified major
+"""  
 @app.route("/get_max_broadening=<string:major>", methods=["GET"])
 def get_broadening_pts(major):
     with driver.session() as session:
@@ -73,7 +83,19 @@ def get_broadening_pts(major):
         data = results.data()
         return jsonify(data)
     
+"""
+Query: Return relevant information about core units within a certain major + any bridging unit required
+Information: 
+String (parsable): unitcode, unitname, type, semester, point_req, incompatibilities, notes
+Interger: level, credit_points
+Arrays: or_req, and_req, unit_req, corequisites
+- or_req = an array of units taken from 
+- and_req = 
+- corequisites = 
 
+AND-OR DOCUMENTATION
+
+"""
 @app.route("/unitInformation/<string:major>/bridging=<string:bridging>/year=<string:year>", methods=["GET"])
 def send_unit_information(major, bridging, year):
     with driver.session() as session:
@@ -104,7 +126,7 @@ def send_unit_information(major, bridging, year):
         WHERE cc.year = "{year}"
         WITH node, COLLECT(DISTINCT r_or.unitcode) as or_req, COLLECT(DISTINCT r_and.unitcode) as and_req, COLLECT(DISTINCT c.unitcode) as corequisites
         WITH node, or_req, and_req, [or_req,and_req] as unit_req, corequisites
-        RETURN node.unitcode as unitcode, node.unitname as unitname, node.type as type, node.semester as semester, node.major as major, node.level as level, node.credit_points as credit_points, node.points_req as points_req, or_req, and_req, unit_req, node.incompatible_units as incompatibilities, corequisites, node.notes as notes
+        RETURN node.unitcode as unitcode, node.unitname as unitname, node.type as type, node.semester as semester, node.level as level, node.credit_points as credit_points, node.points_req as points_req, or_req, and_req, unit_req, node.incompatible_units as incompatibilities, corequisites, node.notes as notes
         ORDER BY level
         """
         
@@ -126,13 +148,14 @@ def get_option_units(major, year):
         WHERE cc.year = "{year}"
         WITH u, COLLECT(DISTINCT r_or.unitcode) as or_req, COLLECT(DISTINCT r_and.unitcode) as and_req, COLLECT(DISTINCT c.unitcode) as corequisites
         WITH u, or_req, and_req, [or_req,and_req] as unit_req, corequisites
-        RETURN u.unitcode as unitcode, u.unitname as unitname, u.type as type, u.semester as semester, u.major as major, u.level as level, u.credit_points as credit_points, u.points_req as points_req, or_req, and_req, unit_req, u.incompatible_units as incompatibilities, corequisites, u.notes as notes
+        RETURN u.unitcode as unitcode, u.unitname as unitname, u.type as type, u.semester as semester, u.level as level, u.credit_points as credit_points, u.points_req as points_req, or_req, and_req, unit_req, u.incompatible_units as incompatibilities, corequisites, u.notes as notes
         ORDER BY level
         """
         results = session.run(query)
         data = results.data()
         return jsonify(data)
 
+#NOTE TO SELF: ADD option unit combos for civil TvT 
 @app.route("/option_combos=<string:major>/year=<string:year>", methods=["GET"])  
 def get_option_combos(major, year): 
      with driver.session() as session:
@@ -145,7 +168,7 @@ def get_option_combos(major, year):
         print(result_list)
 
         # lists of different option units
-        if(major != "SP-ESOFT"):
+        if(major == "SP-EMECH" or major == "SP-ECHEM" or major == "SP-ECIVL"):
             options_a = result_list[0]['options']
             options_b = result_list[1]['options']
             both = options_a+options_b
@@ -193,11 +216,37 @@ def get_option_combos(major, year):
                 combinations.append(finalised)
             print(combinations)
 
+        if major == "SP-ECIVL":
+            # one group A; four group B
+            group_b = list(itertools.combinations(options_b, 4))
+            combos_1 = []
+            for combination in group_b:
+                ga_1 = ["CIVL5550"]
+                ga_2 = ["CIVL5552"]
+                valid_combo = list(combination) 
+                combos_1.append(ga_1 + valid_combo)
+                combos_1.append(ga_2 + valid_combo)
+
+            # two group A; 3 group B
+            group_b = list(itertools.combinations(options_b, 3))
+            combos_2 = []
+            for combination in group_b:
+                valid_combo = list(combination) 
+                valid_combo = valid_combo + options_a
+                combos_2.append(valid_combo)
+            # combine two types of combinations
+            combinations = combos_1 + combos_2
+            print(combinations)
         return jsonify(combinations)
      
 
 # ------------------------------------- TREE VIEW QUERIES ------------------------------------
 
+"""
+Query: Get all parent units of a specified unit that is connected to specified major
+Return: A list paths from chosen unit
+Paths: A list of `unitcodes` that the program has found in its search
+"""  
 @app.route("/prereqs/<string:major>/<string:chosen_unit>", methods=["GET"])
 def get_prereq_units(chosen_unit, major):
     with driver.session() as session:
@@ -213,6 +262,11 @@ def get_prereq_units(chosen_unit, major):
         data = results.data()
         return jsonify(data)
 
+"""
+Query: Get all child units of a specified unit that is connected to specified major
+Return: A list paths from chosen unit
+Paths: A list of `unitcodes` that the program has found in its search
+"""  
 @app.route("/child_units/<string:major>/<string:chosen_unit>", methods=["GET"])
 def get_child_units(chosen_unit, major):
     with driver.session() as session:
